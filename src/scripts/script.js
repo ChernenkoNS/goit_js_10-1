95c94fc1411a85047c2d9ec7a8c388a7c2d5655b
Test <test@MacBook-Air-von-Test.local>
2023-05-24 18:25:38 +0300

    commit-thre

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,39 +1,39 @@
-import "../css/styles.css";
-import API from "./api.js";
-import debounce from "lodash.debounce";
-import Notiflix from "notiflix";
+import '../css/styles.css';
+import API from './api.js';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
 const ref = {
-  countryName: document.getElementById("search-box"),
-  list: document.querySelector(".country-list"),
-  info: document.querySelector(".country-info"),
+  countryName: document.getElementById('search-box'),
+  list: document.querySelector('.country-list'),
+  info: document.querySelector('.country-info'),
 };
 
-ref.countryName.addEventListener("input", debounce(onSubmit, DEBOUNCE_DELAY));
+ref.countryName.addEventListener('input', debounce(onSubmit, DEBOUNCE_DELAY));
 
 function onSubmit(event) {
   const value = event.target.value.trim();
   event.preventDefault();
-  if (value === "") {
+  if (value === '') {
     clearInput();
     return;
   } else
     API.getCountry(value)
-      .then((results) => {
+      .then(results => {
         if (results.length === 0)
-          throw new Error("Oops, there is no country with that name");
+          throw new Error('Oops, there is no country with that name');
         if (results.length > 10) {
           Notiflix.Notify.info(
-            "Too many matches found. Please enter a more specific name."
+            'Too many matches found. Please enter a more specific name.'
           );
           return;
         }
         const curentFun = results.length === 1 ? createInfo : createList;
         const markup = results.reduce(
           (markup, result) => markup + curentFun(result),
-          " "
+          ' '
         );
 
         if (results.length === 1) {
@@ -54,7 +54,7 @@ function createList({ name, flags }) {
 }
 
 function createInfo({ name, flags, capital, population, languages }) {
-  languages = Object.values(languages).join(", ");
+  languages = Object.values(languages).join(', ');
   return `
   <h2>${name.common} (${name.official})</h2>
   <img src="${flags.svg}" alt="${flags.alt}" width="150px">
@@ -65,20 +65,22 @@ function createInfo({ name, flags, capital, population, languages }) {
 }
 
 function updateList(markup) {
-  ref.info.innerHTML = "";
+  ref.info.innerHTML = '';
   ref.list.innerHTML = markup;
 }
 
 function updateInfo(markup) {
-  ref.list.innerHTML = "";
+  ref.list.innerHTML = '';
   ref.info.innerHTML = markup;
 }
 
 function clearInput() {
-  ref.list.innerHTML = "";
-  ref.info.innerHTML = "";
+  ref.list.innerHTML = '';
+  ref.info.innerHTML = '';
 }
 
+
+
 function onError(err) {
-  Notiflix.Notify.failure("Oops, there is no country with that name");
+  Notiflix.Notify.failure('Oops, there is no country with that name');
 }
